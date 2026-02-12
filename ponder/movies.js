b0e3f5716f936e91e9b2a832d528dc91671ef53e
hunterrowley67 <hunterrowley67@gmail.com>
2026-02-11 17:47:25 -0700

    fix error in js

--- ponder/movies.js
+++ ponder/movies.js
@@ -49,17 +49,13 @@ const movies = [
         const article = document.createElement('article')
         article.className ='movie';
         let html = 
-            `<article class="movie">
-                <h2>${movie.title}</h2>
+                `<h2>${movie.title}</h2>
                 <img src=${movie.imgSrc} alt=${movie.imgAlt}>
                 <p><strong>Release Date:</strong> ${movie.date}</p>
                 <p><strong>Recommended Age:</strong> ${movie.ages}</p>
                 <p><strong>Genre:</strong> ${movie.genre}</p>
                 <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
-                <p id='desc'>${movie.description}</p>
-            </article>`
+                <p id='desc'>${movie.description}</p>`;
         article.innerHTML = html;
         movieList.appendChild(article);
-        
     });
-          
